3646bc36ca7c09466f0cb01b8009756f9bf21425
GBC23 <syi2374@outlook.com>
2023-12-07 04:15:47 -0500

    adsf

---
+++ day6/2_reeborg's_world.py
@@ -0,0 +1,28 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+turn_left()
+move()
+
+turn_right()
+move()
+
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
---
+++ day6/3_reeborg_hurdle.py
@@ -0,0 +1,42 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+turn_left()
+move()
+turn_right()
+move()
+
+turn_right()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
